20b64d68d2413ab6cea0eeeabd44324918622883
Janishi <106867667+Janishi@users.noreply.github.com>
2022-09-09 01:22:54 +0400

    burger bar animation

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // burger bar
 
-let burger = document.getElementById('burger-bar');
+let burger = document.getElementById('toggle-wrapper');
 let navigation = document.getElementById ('nav-id');
 
-burger=addEventListener('click', function(){
-
+burger.addEventListener('click', function(){
+    burger.classList.toggle('active-burger');
     navigation.classList.toggle('active');
 });
