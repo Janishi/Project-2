3f6179312c0c9d262f25a26464ce07dd69ac20fc
Janishi <106867667+Janishi@users.noreply.github.com>
2022-09-09 23:10:12 +0400

    added nav features

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ burger.addEventListener('click', function(){
     burger.classList.toggle('active-burger');
     navigation.classList.toggle('active');
 });
+
+let projects = document.getElementById('project');
+let works=document.getElementById('work');
+let prowin = document.getElementById('pro-win');
+let workWindow = document.getElementById('work-win')
+
+projects.addEventListener('click', function(){
+    prowin.classList.toggle('pro-active');
+    workWindow.classList.remove('work-active');
+});
+
+works.addEventListener('click', function(){
+    workWindow.classList.toggle('work-active');
+    prowin.classList.remove('pro-active');
+});
